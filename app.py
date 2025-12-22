bb1d0f65bea47596d557407967000a60d2e43c01
markAlejo <mark.anthony552@yahoo.com>
2025-12-22 13:45:39 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 #import flask into the app
 from flask import Flask
 
+# Creates an instance of the flask app
 app = Flask(__name__)
 
+# Define a route for the root URL '/' 
+# and link it to the function below
 @app.route("/")
 def home(): 
     return "ServerSeal backend is running"
 
+#checks if the file is run directly,
+# then start the flask development server
 if __name__ == "__main__":
     app.run(debug=True)
-#Check "health" check ro a route
-#Example ServerSeal backend ir running 
+
 
